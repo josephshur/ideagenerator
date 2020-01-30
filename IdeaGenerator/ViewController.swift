31a80a385a35f7d0f26ec9e04fa628ffec9e9dca
Joseph Shur <joehshur@gmail.com>
2020-01-29 18:21:44 -0800

    Initial Commit

---
+++ IdeaGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdeaGenerator
+//
+//  Created by Joseph Shur on 1/29/20.
+//  Copyright © 2020 Joseph Shur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
